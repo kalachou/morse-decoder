cc80dbbb9455a2928c22bffe3e91248439fa0197
kalachou <mvkalach>
2019-09-28 16:26:04 +0300

    morse-decoder(feat): implementation

---
+++ src/index.js
@@ -0,0 +1,69 @@
+const MORSE_TABLE = {
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
+};
+
+function decode(expr) {
+    let arr = [];
+    let arrLength;
+
+    arr = expr.match(/.{1,10}/g) || [];
+    arrLength = arr.length;
+
+    for (let i = 0; i < arrLength; i++) {
+        if (arr[i] === '**********') {
+            arr[i] = ' ';
+        } else {
+            arr[i] = arr[i].replace(/00/g, '');
+            arr[i] = arr[i].replace(/10/g, '.');
+            arr[i] = arr[i].replace(/11/g, '-');
+        }
+    }
+
+    for (let i = 0; i < arrLength; i++) {
+        if (arr[i] !== ' ') {
+            arr[i] = MORSE_TABLE[arr[i]];
+        }
+    }
+
+    return arr.join('');
+    
+}
+
+module.exports = {
+    decode
+}
